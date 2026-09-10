43481c17290e22aba7c481470563c83adf51f006
KhushiG721 <khushigadad313@gmail.com>
2026-09-10 12:00:36 +0530

    Added Boolean Type Conversion

---
+++ Basics/Type Conversion/boolean_conversion.py
@@ -0,0 +1,23 @@
+"""
+Demonstrates Boolean type conversion using bool().
+"""
+
+# Numbers
+print("bool(1):", bool(1))
+print("bool(10):", bool(10))
+print("bool(0):", bool(0))
+print("bool(-5):", bool(-5))
+
+
+# Strings
+print("\nbool('Python'):", bool("Python"))
+print("bool(''):", bool(""))
+
+
+# Lists
+print("\nbool([1, 2, 3]):", bool([1, 2, 3]))
+print("bool([]):", bool([]))
+
+
+# None
+print("\nbool(None):", bool(None))
