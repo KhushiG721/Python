05a58f2685b6fa83df063c6ed3c307aeb63d0138
KhushiG721 <khushigadad313@gmail.com>
2026-09-13 12:59:56 +0530

    Added Multi Line Comments Examples

---
+++ Basics/Comments-Docstrings/multi_line_comments.py
@@ -0,0 +1,13 @@
+"""
+Demonstrates writing multiple lines of comments.
+"""
+
+# The following section stores
+# information about a product
+# and displays it to the user.
+
+product_name = "Backpack"
+price = 1200
+
+print("Product:", product_name)
+print("Price:", price)
